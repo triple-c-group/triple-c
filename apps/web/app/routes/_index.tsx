3c5060fdf6d81bce2b5c09f0b6c0fd89a61eba96
leo <leo555pratipnatsiri@gmail.com>
2024-02-22 21:45:34 +0700

    Add first draft of main page

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,30 +1,71 @@
 import type { MetaFunction } from "@remix-run/node";
+import Bg_blue from "../component/Bg_blue";
+import TopBar from "../component/topBar_button";
+import PhotoCards_scroll from "../component/photoCards_scroll";
+import App_news from "../component/App_news";
+import Search_button from "~/component/Search_button";
+import Search_buttonPop from "~/component/Search_buttonPop";
+import { useState, useEffect } from "react";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
-  return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
+  return [
+    { title: "Tripple C" },
+  ];
 };
 
 export default function Index() {
+
+  const [topbarScroll, setTopbarScroll] = useState(false);
+  const [searchButton, setSearchButton] = useState(false);
+  const [thaiLan, setThaiLan] = useState(false)
+
+  const deactivateSearch = () => setSearchButton(false);
+
+  useEffect(function mount() {
+      function changeBackground() {
+        console.log(window.scrollY);
+        if (window.scrollY >= 30) {
+          setTopbarScroll(true)
+        } else {
+          setTopbarScroll(false)
+        }
+      }
+      window.addEventListener("scroll", changeBackground);
+      return function unMount() {
+        window.removeEventListener("scroll", changeBackground);
+      };
+    });
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main className="relative bg-gray-200 ">
+              <div>
+                <Bg_blue topbarScroll={topbarScroll}/>
+              </div>
+              <div>
+                <PhotoCards_scroll thaiLan={thaiLan}/>
+              </div>
+              <div>
+                <Search_button setSearchButton={setSearchButton}/>
+              </div>
+              <div>
+                <TopBar 
+                  topbarScroll={topbarScroll} 
+                  setThaiLan={setThaiLan} 
+                  thaiLan={thaiLan}
+                />
+              </div>
+              <div>
+                <App_news thaiLan={thaiLan}/>
+              </div>
+              <div>
+                <Search_buttonPop 
+                  thaiLan={thaiLan}
+                  searchButton={searchButton}
+                  setSearchButton={setSearchButton}
+                />
+              </div>
+
+    </main>
   );
 }
